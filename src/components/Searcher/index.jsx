57ddf417782a5e2ce6d33730ac043375c20adbec
Andres Maneiro <amaneiro7@gmail.com>
2023-04-21 18:02:47 -0400

    check

---
+++ src/components/Searcher/index.jsx
@@ -0,0 +1,11 @@
+import { IconButton, Stack, TextField } from "@mui/material";
+import React from "react";
+
+export function Searcher() {
+    return (
+        <Stack>
+            <TextField></TextField>
+            <IconButton></IconButton>
+        </Stack>
+    )
+}
